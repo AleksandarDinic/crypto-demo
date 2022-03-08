02152b8047c7a0f6e4d04153009faea85310cb3b
Aleksandar Dinic <a.dinic@icloud.com>
2022-03-08 13:48:03 +0100

    Initial commit

---
+++ CryptoDemo/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  CryptoDemo
+//
+//  Created by Aleksandar Dinic on 03/08/2022.
+//  Copyright © 2022 Aleksandar Dinic. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CryptoDemo/CryptoDemoApp.swift
@@ -0,0 +1,18 @@
+//
+//  CryptoDemoApp.swift
+//  CryptoDemo
+//
+//  Created by Aleksandar Dinic on 03/08/2022.
+//  Copyright © 2022 Aleksandar Dinic. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct CryptoDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
